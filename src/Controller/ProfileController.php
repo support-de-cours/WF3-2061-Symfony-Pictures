9e6b5ee93be7abf8108914b7cf2e6259f17c7777
Arnaud BODEL <contact@osw3.net>
2023-05-31 15:24:15 +0200

    -

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomepageController extends AbstractController
+class ProfileController extends AbstractController
 {
-    #[Route('/', name: 'app_homepage')]
+    #[Route('/profile', name: 'app_profile')]
     public function index(): Response
     {
-        return $this->render('pages/homepage/index.html.twig', [
+        return $this->render('pages/profile/index.html.twig', [
         ]);
     }
 }
