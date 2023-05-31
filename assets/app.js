5d74f42e902ebf0b3e76bfd96f7fb2dfbf416b34
Arnaud BODEL <contact@osw3.net>
2023-05-31 15:05:13 +0200

    -

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,9 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
 // start the Stimulus application
-import './bootstrap';
+import './bootstrap'; // Fichier /assets/bootstrap.js
+import 'bootstrap'; // Module Bootstrap /node_modules/bootstrap/
--- webpack.config.js
+++ webpack.config.js
@@ -55,6 +55,10 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = '3.23';
     })
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+    })
 
     // enables Sass/SCSS support
     //.enableSassLoader()
